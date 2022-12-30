714fb46d229ae36ef94de9c121fd376d89fcef26
Nikita U <nikitajc@mail.ru>
2022-12-30 13:49:51 +0300

    Some data

---
+++ src/test/java/postmanTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+
+public class postmanTest {
+    @Test
+    void TestPostman() {
+// Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("schema.json"))
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
